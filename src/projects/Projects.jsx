0160cb8bc25699e1156b2db9c614183c03229036
Ramesh Reddy <52284933+rameshx@users.noreply.github.com>
2022-01-31 16:42:04 +0530

    finish projects section

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -1,28 +1,106 @@
 import { useRef } from 'react';
-
-import Fab from '@mui/material/Fab';
 import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined';
 import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
 import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
+import FaceOutlinedIcon from '@mui/icons-material/FaceOutlined';
+import TerrainOutlinedIcon from '@mui/icons-material/TerrainOutlined';
+import LunchDiningOutlinedIcon from '@mui/icons-material/LunchDiningOutlined';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
 import './Projects.scss';
 
-const Project = () => {
+const PROJECTS = [
+  {
+    className: 'primary',
+    icon: <TimerOutlinedIcon />,
+    name: 'Timer',
+    description:
+      'A React and TypeScript timer application inspired by the timer in android clock app.',
+    link: {
+      github: 'https://github.com/rameshx/timer',
+      live: 'https://rameshx.github.io/timer/',
+    },
+  },
+  {
+    className: 'secondary',
+    icon: <BusinessCenterOutlinedIcon />,
+    name: 'Portfolio',
+    description:
+      'This portfolio built using React, SASS (SCSS) and material icons.',
+    link: {
+      github: 'https://github.com/rameshx/portfolio',
+      live: '#',
+    },
+  },
+  {
+    className: 'tertiary',
+    icon: <FaceOutlinedIcon />,
+    name: 'Face Detector',
+    description:
+      'A cool face detection app built using React, PostgreSQL, Clarifai API, Node.js.',
+    link: {
+      github: 'https://github.com/rameshx/face-detector',
+      live: 'https://facerecognition-frontend-app.herokuapp.com/',
+    },
+  },
+  {
+    className: 'quaternary',
+    icon: <TerrainOutlinedIcon />,
+    name: 'Natours',
+    description:
+      'A responsive nature tours landing page built for learning SASS and advanced CSS.',
+    link: {
+      github: 'https://github.com/rameshx/advanced-css',
+      live: 'https://rameshx.github.io/advanced-css/',
+    },
+  },
+  {
+    className: 'quinary',
+    icon: <LunchDiningOutlinedIcon />,
+    name: 'Burger Builder',
+    description:
+      'A React application that allows users to customize and order burgers.',
+    link: {
+      github: 'https://github.com/rameshx/burger-builder',
+      live: 'https://react-burger-builder-d0989.firebaseapp.com/',
+    },
+  },
+];
+
+const Project = ({
+  className,
+  icon,
+  description,
+  name,
+  link: { live, github },
+}) => {
   return (
-    <div className="project">
-      <span className="icon-wrapper">
-        <TimerOutlinedIcon />
-      </span>
+    <div className={`project ${className}`}>
+      <span className="icon-wrapper">{icon}</span>
       <div className="body">
-        <p className="description">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero quae
-          odit eius explicabo quibusdam sapiente. Dolorum reprehenderit illo,
-          aliquid eveniet laborum.
-        </p>
+        <p className="description">{description}</p>
         <hr />
-        <h5 className="name">React Timer</h5>
+        <h5 className="name">{name}</h5>
+        <span>
+          <a
+            className="link"
+            href={github}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          &nbsp;|&nbsp;
+          <a
+            className="link"
+            href={live}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Live
+          </a>
+        </span>
       </div>
     </div>
   );
@@ -42,35 +120,25 @@ export const Projects = () => {
           slidesPerView={'auto'}
           spaceBetween={16}
         >
-          <SwiperSlide>
-            <Project />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Project />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Project />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Project />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Project />
-          </SwiperSlide>
+          {PROJECTS.map((project) => (
+            <SwiperSlide key={project.name}>
+              <Project {...project} />
+            </SwiperSlide>
+          ))}
         </Swiper>
         <div className="projects-navigation">
-          <Fab
-            color="primary"
+          <button
+            className="fab"
             onClick={() => swiperRef.current.swiper.slidePrev()}
           >
             <ArrowBackOutlinedIcon />
-          </Fab>
-          <Fab
-            color="secondary"
+          </button>
+          <button
+            className="fab"
             onClick={() => swiperRef.current.swiper.slideNext()}
           >
             <ArrowForwardOutlinedIcon />
-          </Fab>
+          </button>
         </div>
       </div>
     </section>
